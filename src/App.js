1d677fe0953e498ac5369480d3b2ba3bf0a9b29f
oleksandr.gurov <oleksandr.gurov@vaimo.com>
2020-05-06 14:24:52 +0300

    Create api service class

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const App = () => {
-  return (
-    <div className="container">
-      <h1>Star DB</h1>
-    </div>
-  );
-};
+class App extends Component {
+  state = {};
+
+  render() {
+    return (
+      <div className="container">
+        <h1>Star DB</h1>
+      </div>
+    );
+  }
+}
 
 export default App;
